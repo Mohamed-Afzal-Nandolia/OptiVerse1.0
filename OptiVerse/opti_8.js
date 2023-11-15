e4a955cc0acd227fd2f82e0b2b988b9243d73753
Mohamed-Afzal-Nandolia <afzalcool601+github@gmail.com>
2023-11-15 13:58:54 +0530

    Posting thoughts on the database is done;
    In this commit I have connected the database and the post button and the field is well so when the user types something in the user field that whatever the content is after clicking the button it gets transferred to the database

--- OptiVerse/opti_8.js
+++ OptiVerse/opti_8.js
@@ -1,9 +1,10 @@
+//first
 const express = require('express');
 const session = require('express-session');
 const mysql = require("mysql");
 const qs = require("querystring");
 const fs = require("fs");
-
+//second
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: "localhost",
@@ -12,17 +13,20 @@ const pool = mysql.createPool({
     database: "optiverse",
     port: 3307,
 });
+//third
+var app = express();
 
-const app = express();
+//this will make all the public folder files static, so we dont have to use the app.get() for forgotpassword and terms and conditions.
+app.use(express.static('public')); 
 
-app.use(express.static('public')); // Add this line
-
+//fourth
 app.use(session({
     secret: 'your secret key',
     resave: false,
     saveUninitialized: true,
 }));
 
+//sixth
 app.get('/', (req, res) => {
     if (req.session.loggedIn) {
         res.sendFile(__dirname + '/homepage.html');
@@ -30,7 +34,33 @@ app.get('/', (req, res) => {
         res.sendFile(__dirname + '/loginpage.html');
     }
 });
+app.get('/post', (req, res) => {
+    if (req.session.loggedIn) {
+        res.sendFile(__dirname + '/post.html');
+    } else {
+        res.redirect('/');
+    }
+});
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        res.redirect('/loginpage.html');
+    });
+});
+app.get('/loginpage.html', (req, res) => {
+    res.sendFile(__dirname + '/loginpage.html');
+});
 
+/* Because we have use app.use(express.static('public')); this will make all the public folder files static,
+so we dont have to use the app.get() for forgotpassword and terms and conditions.
+app.get('/forgotpassword.html', (req, res) => {
+    res.sendFile(__dirname + '/forgotpassword.html');
+});
+*/
+
+// fifth, routes
 app.post('/', (req, res) => {
     let formdata = "";
     req.on("data", (chunk) => {
@@ -42,6 +72,38 @@ app.post('/', (req, res) => {
 
         const query = "SELECT * FROM users WHERE email = ? AND password = ?";
         const values = [data.uemail, data.upass];
+        req.session.usermail = data.uemail;
+
+        pool.getConnection((err, connection) => {
+            if (err) throw err;
+
+            connection.query(query, values, (err, result) => {
+                connection.release();   
+
+                if (err) throw err;
+
+                if (result.length > 0) {
+                    req.session.loggedIn = true;
+                    res.redirect('/');//to sixth
+                } else {
+                    res.send("User is not registered");
+                }
+            });
+        });
+    });
+});
+// fifth, routes
+app.post('/post-thought', (req, res) => {
+    let formdata = "";
+    req.on("data", (chunk) => {
+        formdata += chunk;
+    });
+
+    req.on("end", () => {
+        const data = qs.parse(formdata);
+
+        const query = "INSERT INTO userposts (username, post) VALUES (?, ?)";
+        const values = [req.session.usermail, data.thought]; // replace 'req.session.username' with the actual username
 
         pool.getConnection((err, connection) => {
             if (err) throw err;
@@ -51,17 +113,11 @@ app.post('/', (req, res) => {
 
                 if (err) throw err;
 
-                if (result.length > 0) {
-                    req.session.loggedIn = true;
-                    res.redirect('/');
-                } else {
-                    res.send("User is not registered");
-                }
+                res.redirect('/');
             });
         });
     });
 });
-
 app.post('/register', (req, res) => {
     let formdata = "";
     req.on("data", (chunk) => {
@@ -87,7 +143,7 @@ app.post('/register', (req, res) => {
         });
     });
 });
-
+// sixth
 app.listen(1200, () => {
     console.log("Server is listening on port 1200");
 });
